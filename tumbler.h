8c1f6308916421c9521faabac6a549263f5279be
Quan <qyl728@sjtu.edu.cn>
2023-12-29 06:16:15 +0800

    balls collaps with tumblers are fixed by using boxxing!

--- tumbler.h
+++ tumbler.h
@@ -96,90 +96,100 @@ public:
         }
 
     }
-    glm::vec3 calNormal(const std::vector<glm::vec3>& face) {
-        glm::vec3 v1 = face[0];
-        glm::vec3 v2 = face[1];
-        glm::vec3 v3 = face[2];
+    void updateModelMatrixWithNewRotation(const glm::vec3& angles) {
+        // 首先，重置模型矩阵为单位矩阵
+        modelMatrix = glm::mat4(1.0f);
 
-        // 计算两个边向量
-        glm::vec3 edge1 = v2 - v1;
-        glm::vec3 edge2 = v3 - v1;
-
-        // 计算法线（使用右手法则）
-        glm::vec3 normal = glm::normalize(glm::cross(edge1, edge2));
-        return normal;
+        // 依次按照x、y、z轴旋转
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(angles.x), glm::vec3(1.0f, 0.0f, 0.0f)); // 绕x轴旋转
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(angles.y), glm::vec3(0.0f, 1.0f, 0.0f)); // 绕y轴旋转
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(angles.z), glm::vec3(0.0f, 0.0f, 1.0f)); // 绕z轴旋转
     }
-    float distanceToPlane(const std::vector<glm::vec3>& face, const glm::vec3& center) {
-        // 计算法线（使用右手法则）
-        glm::vec3 normal = calNormal(face);
+    void stayStable() {
+        glm::vec3 angle = extractRotationFromModelMatrix();
+        cout<<"当前倾斜的角度"<<angle.x<<" "<<angle.y<<" "<<angle.z<<endl;
+        const float maxTiltAngle = 60.0f;  // 设置一个最大的倾斜角度，单位为度
 
-        // 现在，计算球心到平面的距离
-        float distanceToPlane = glm::dot(center - face[0], normal);
-        return distanceToPlane;
-    }
-    bool checkSpherePlaneCollision(const glm::vec3& center, float radius, const std::vector<glm::vec3>& face) {
-       
-        // 计算法线（使用右手法则）
-        float d= distanceToPlane(face, center);
-
-        // 如果球心到平面的距离小于球的半径，则存在碰撞
-        if (glm::abs(d) <= radius) {
-            return true;
+        // 检查x轴的倾斜
+        if (glm::abs(angle.x) > maxTiltAngle) {
+            angle.x = (angle.x > 0) ? maxTiltAngle-2 : 2-maxTiltAngle;
         }
-        return false;
+
+        // 检查y轴的倾斜
+        if (glm::abs(angle.y) > maxTiltAngle) {
+            angle.y = (angle.y > 0) ? maxTiltAngle - 2 : 2 - maxTiltAngle;
+        }
+
+        // 检查z轴的倾斜
+        if (glm::abs(angle.z) > maxTiltAngle) {
+            angle.z = (angle.z > 0) ? maxTiltAngle - 2 : 2 - maxTiltAngle;
+        }
+        angularVelocity *=-1;
+        // 更新模型矩阵或其他相关数据
+        updateModelMatrixWithNewRotation(angle);
+       
+        cout << "修正后的倾斜角度" << angle.x << " " << angle.y << " " << angle.z << endl;
     }
     void updateBallToTumbler(glm::vec3 cPoint) {
         glm::vec4 GravityPoint = modelMatrix * glm::vec4(gravityPoint, 1.0f);
         glm::vec3 MyGravityPoint = glm::vec3(GravityPoint);
-        glm::vec3 deltaAngleV = glm::cross(glm::vec3(0, -ballGravity, 0), cPoint - MyGravityPoint);
-        angularVelocity -= deltaAngleV / momentOfInertia;
+        glm::vec4 ccPoint = modelMatrix * glm::vec4(cPoint, 1.0f);
+        glm::vec3 MyCPoint = glm::vec3(ccPoint);
+        glm::vec3 deltaAngleV = glm::cross(glm::vec3(0, -ballGravity, 0), MyCPoint - MyGravityPoint);
+        angularVelocity -= glm::vec3(deltaAngleV.x / 10, deltaAngleV.y / 10, deltaAngleV.z / 10);
     }
-    //记录了碰撞点位置
-    void checkBallCollapsing(Model& model) {
-        vector<glm::vec3> vers = model.boundingBox.getVertices();
-        //对每个点做模型矩阵变换
-        for (int i = 0; i < 8; i++) {
-            glm::vec3 ver = vers[i];
-            glm::vec4 newBox = modelMatrix * glm::vec4(ver, 1.0f);
-            vers[i] = glm::vec3(newBox);
+    bool checkSphereCubeIntersection(const glm::vec3& ballCenter, float ballRadius, const glm::vec3& minPoint, const glm::vec3& maxPoint, glm::vec3& intersectionPoint) {
+        // 计算球心到正方体的最近点
+        glm::vec3 closestPoint(0.0f);
+
+        if (ballCenter.x < minPoint.x) closestPoint.x = minPoint.x;
+        else if (ballCenter.x > maxPoint.x) closestPoint.x = maxPoint.x;
+        else closestPoint.x = ballCenter.x;
+
+        if (ballCenter.y < minPoint.y) closestPoint.y = minPoint.y;
+        else if (ballCenter.y > maxPoint.y) closestPoint.y = maxPoint.y;
+        else closestPoint.y = ballCenter.y;
+
+        if (ballCenter.z < minPoint.z) closestPoint.z = minPoint.z;
+        else if (ballCenter.z > maxPoint.z) closestPoint.z = maxPoint.z;
+        else closestPoint.z = ballCenter.z;
+
+        // 计算球心到最近点的距离
+        float distance = glm::length(ballCenter - closestPoint);
+
+        // 如果这个距离小于或等于球的半径，则球与正方体相交
+        if (distance <= ballRadius) {
+            // 交点是最近的点
+            intersectionPoint = closestPoint;
+            return true;
         }
-        std::vector<std::vector<glm::vec3>> faces = {
-            // 前面
-            {vers[0], vers[1], vers[3], vers[2]},
 
-            // 后面
-            {vers[4], vers[5], vers[7], vers[6]},
+        return false;
+    }
 
-            // 左侧
-            {vers[0], vers[2], vers[6], vers[4]},
+ 
 
-            // 右侧
-            {vers[1], vers[3], vers[7], vers[5]},
-
-            // 顶部
-            {vers[2], vers[3], vers[7], vers[6]},
-
-            // 底部
-            {vers[0], vers[1], vers[5], vers[4]}
-        };
+ 
+    void checkBallCollapsing(Model& model) {
+        glm::vec3 min = glm::vec3(model.boundingBox.minX, model.boundingBox.minY, model.boundingBox.minZ);
+        glm::vec3 max = glm::vec3(model.boundingBox.maxX, model.boundingBox.maxY, model.boundingBox.maxZ);
+      
         for (int i = 0; i < balls.size(); i++) {
-           //如果ball跟包围盒的八个顶点构成的立方体有交点，则会发生碰撞，需要速度反向，并记录交点坐标，根据小球撞击前的速度决定tumbler的状态变化
             glm::vec3 ballCenter = balls[i].position;
-            for (const auto& face : faces) {
-                if (checkSpherePlaneCollision(balls[i].position, ballRadius, face)) {
-                    // 碰撞点是球的中心减去与平面的距离乘以法线
-                    glm::vec3 collisionPoint = balls[i].position - ballRadius * calNormal(face);
-                    //更新不倒翁的角速度
-                    updateBallToTumbler(collisionPoint);
-                    // 反射速度
-                    balls[i].color = glm::vec3(1.0f, 0.0f, 0.0f);
-                    balls[i].velocity *= -0.8;
-                }
+            glm::vec4 modelCoords = glm::inverse(modelMatrix) * glm::vec4(ballCenter, 1.0f);
+            ballCenter = glm::vec3(modelCoords);
+            //检测球心是否在包围盒内
+            glm::vec3 collapsPoint = glm::vec3(-1, -1, -1);
+            if (checkSphereCubeIntersection(ballCenter, ballRadius, min, max, collapsPoint)) {
+                balls[i].color = glm::vec3(1.0f, 0.0f, 0.0f);
+                balls[i].velocity *= -0.8;
+				balls[i].position += balls[i].velocity * deltaTime;
+				updateBallToTumbler(collapsPoint);
             }
 
         }
-
-	}
+    }
+             
     void upToFloor(Model& model) {
         vector<glm::vec3> vers =model.boundingBox.getVertices();
         float miny = vers[0].y;
@@ -236,7 +246,7 @@ public:
         //return angleAcc;
         glm::vec3 angle = extractRotationFromModelMatrix();
         cout << "当前倾斜的角度" << angle.x << " " << angle.y << " " << angle.z << endl;
-        angularAcceleration = glm::vec3(angle.x/100, angle.y/100,angle.z/100);
+        angularAcceleration = glm::vec3(angle.x/10, angle.y/10,angle.z/10);
     }
     void updateTumbler(Model model) {
 
@@ -245,9 +255,10 @@ public:
             angularVelocity += angularAcceleration * deltaTime;
             return;
         }
-        cout<<"需要update"<<endl;
+       // cout<<"需要update"<<endl;
 		// 更新角度
 		modelMatrix = glm::rotate(modelMatrix, glm::length(angularVelocity) * 2*deltaTime, angularVelocity);
+        //stayStable();
         computeAngleAcc(model);
         // 更新角速度
         angularVelocity += angularAcceleration * deltaTime;
